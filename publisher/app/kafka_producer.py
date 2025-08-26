7864cb93cfc76b0e0aee7aa314a4fb089887bd20
benjyp2003 <benjypfeffer@gmail.com>
2025-08-26 23:21:11 +0300

    Update Kafka producer configuration to use Dockerized Kafka server and refactor message publishing methods

--- publisher/app/kafka_producer.py
+++ publisher/app/kafka_producer.py
@@ -7,20 +7,18 @@ class Producer:
     def __init__(self):
         self.producer = self.get_producer_config()
 
-
-    def publish_message_with_key(self, topic,key, message):
-        self.producer.send(topic, key=key, value=message)
-
     def get_producer_config(self):
         # The Producer object requires the Kafka server, Json serializer
-        producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+        producer = KafkaProducer(bootstrap_servers=['kafka:9092'],
                                  value_serializer=lambda x:
                                  json.dumps(x).encode('utf-8'))
         return producer
 
-    def publish(self,   topic, event):
-        self.producer.send(topic, event)
+    def publish_message_with_key(self, topic, key, message):
+        self.producer.send(topic, key=key, value=message)
 
+    def publish(self, topic, event):
+        self.producer.send(topic, event)
 
     def flush(self):
         self.producer.flush()
