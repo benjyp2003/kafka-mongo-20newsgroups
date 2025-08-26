6a9b3bdc2d2c38c772aee7bfbaf8529c4bed2fcb
benjyp2003 <benjypfeffer@gmail.com>
2025-08-26 23:20:22 +0300

    Refactor Consumer class to improve Kafka consumer configuration and remove unused message consumption method

--- interesting_subscriber/app/consumer.py
+++ interesting_subscriber/app/consumer.py
@@ -6,33 +6,14 @@ import json
 class Consumer:
 
     @staticmethod
-    def get_consumer_events(topic = ''):
+    def get_consumer_events(topic):
+        """Returns a Kafka consumer for the specified topic."""
         # The consumer object contains the topic name, json deserializer,Kafka servers
-        # and kafka time out in ms, Stops the iteration if no message after 1 sec
         consumer = KafkaConsumer(topic,
                                  group_id='interesting-subscriber-group',
                                  value_deserializer=lambda m: json.loads(m.decode('ascii')),
-                                 bootstrap_servers=['localhost:9092'],
-                                 consumer_timeout_ms=10000)
+                                 bootstrap_servers=['kafka:9092'],
+                                 auto_offset_reset='earliest')
         return consumer
 
 
-    def consume_messages(self, topic: str = "interesting_categories"):
-        try:
-            events = self.get_consumer_events(topic)
-            all_messages = []
-            for msg in events:
-                doc = {
-                    "topic": msg.topic,
-                    "partition": msg.partition,
-                    "offset": msg.offset,
-                    "key": msg.key.decode("utf-8") if msg.key else None,
-                    "value": msg.value,
-                    "ts": datetime.now(timezone.utc),  # receipt timestamp (UTC, tz-aware)
-                }
-                all_messages.append(doc)
-
-            return all_messages
-
-        except Exception as e:
-            raise Exception(f"Error consuming messages from topic {topic}: {e}")
